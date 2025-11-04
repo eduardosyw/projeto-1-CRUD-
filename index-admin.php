bbc18b1b3c424ef7049c7f809bd1ee4f3265b324
eduardosyw <eduardosyw@gmail.com>
2025-11-04 19:52:35 -0300

    Add files via upload

---
+++ index-admin.php
@@ -0,0 +1,17 @@
+<?php
+echo "<h1>Painel Administrativo</h1>";
+?>
+
+<nav>
+    <ul>
+        <li><a href="clientes-cadastro-form.php">Cadastrar Cliente</a></li>
+        <li><a href="clientes-lista.php">Listar Clientes</a></li>
+
+        <li><a href="posts-lista.php">Listar Posts</a></li>
+        <li><a href="posts-cadastro-form.php">Cadastrar Post</a></li>
+
+        <li><a href="#">Contatos</a></li>
+        
+        <li><a href="#">Outros</a></li>
+</ul>
+</nav>
